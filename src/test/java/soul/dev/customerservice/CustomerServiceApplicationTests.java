55768869b5056ee6a6c24ebfedf34f9780a23824
soulaiman ghailan <soulaiman8ghailan@gmail.com>
2024-11-27 11:42:07 +0100

    init project

---
+++ src/test/java/soul/dev/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package soul.dev.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
